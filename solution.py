5c6dc3ae2149b0622d45af1505b8126bd838f45f
Matthew <mattalexshaw@gmail.com>
2020-04-30 20:33:09 +0100

    add template solution.py

---
+++ solution.py
@@ -0,0 +1,15 @@
+def setup_sequence(sequence_directory):
+    # TODO: These do not have to be completed in order
+    # 1. Find animatics
+    # 2. Find board sequences
+    # 3. Create shot folders
+    # 4. Create child folders inside the shot
+    # 5. Copy animatic
+    # 6. Copy boards
+    pass
+
+
+if __name__ == '__main__':
+    # TODO: Get the filepath passed to the script, warn the user if they use it wrong
+    path = ""
+    setup_sequence(path)
